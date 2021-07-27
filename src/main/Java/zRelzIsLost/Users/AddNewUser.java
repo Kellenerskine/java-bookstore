38f2eab8e9a2e9908d374d5cbced4f90c3e6671a
Kellenerskine <kellenae129@gmail.com>
2021-07-27 00:18:56 -0400

    mostly done with Luhn algorithm, the loops are glitchy though

--- src/main/Java/zRelzIsLost/Users/AddNewUser.java
+++ src/main/Java/zRelzIsLost/Users/AddNewUser.java
@@ -18,13 +18,13 @@ public class AddNewUser {
         String username = user.nextLine(); //to make browsing the store casually easier?
 
         //payment methods
-        System.out.println("What payment method would you like to use?\n If Paypal enter 'a', If credit card enter 'b'\n-->");
+        System.out.print("What payment method would you like to use?\n If Paypal enter 'a', If credit card enter 'b'\n-->");
         String paymentMethChoice = user.nextLine();
 
         //this feels stupid and unnecessary
         String paypalEmail = "";
         String cardholderName = "";
-        int creditNum = 0;
+        long creditNum = 0;
         int expiryMonth = 0;
         int expiryYear = 0;
         int threeNumThingy = 0;
@@ -37,8 +37,52 @@ public class AddNewUser {
         }else{
             System.out.print("Enter the Card Holder name: ");
             cardholderName = user.nextLine();
-            System.out.print("Enter your credit card number: "); //TODO: use lonz(?) to verify
-            creditNum = user.nextInt(); //add lots of of conditionals, but not at 2 am lol.
+            System.out.print("Enter your credit card number: "); //TODO: use Luhn algorithm to check for mistakes
+            creditNum = user.nextLong(); // of of conditionals, but not at 2 am lol.
+
+            //Luhn algorithm implementation according to https://java2blog.com/luhn-algorithm-java/
+            //comments will be labeled in steps according to website
+            int d = 0;
+            while(d < 1) {
+                if(String.valueOf(creditNum).length() == 16){
+                    //passed basic length test, onto the algorithm
+                    for(int i = 16; i > 0;i--){ //step 1
+                        int productOfNums = 0;
+                        if((i % 2) != 0){
+                            String cnS = String.valueOf(creditNum);
+                            char currentNum = cnS.charAt(i);
+                            String randTransfer = String.valueOf(currentNum);
+                            int numberInt = Integer.parseInt(randTransfer);
+                            int result = numberInt * 2;                         //TODO: something in this loop is broken, repeats error msg tons then goes on to expiry dates
+                            if(result > 9){ //step 2
+                                String strOfResult = String.valueOf(result);
+                                char num1 = strOfResult.charAt(0);
+                                String strNum1 = String.valueOf(num1);
+                                int intOfNum1 = Integer.parseInt(strNum1);
+                                char num2 = strOfResult.charAt(1);
+                                String strNum2 = String.valueOf(num2);
+                                int intOfNum2 = Integer.parseInt(strNum2);
+                                int result2 = intOfNum1 + intOfNum2;
+                                productOfNums += result2; //productOfNums is step 3
+                            }else{
+                                productOfNums += result;
+                            }
+                        }else{
+                            productOfNums += i;
+                        }
+                        if((productOfNums % 10) != 0){ //step 4 (final step)
+                            System.out.println("Please enter a valid credit card number.");
+                        }else{
+                            d += 10;
+                        }
+                    }
+                }else{
+                    System.out.println("Please enter a valid credit card number.");
+                }
+            }
+
+
+
             System.out.print("Card expiry month: ");
             expiryMonth = user.nextInt();
             System.out.print("Card expiry year: ");
